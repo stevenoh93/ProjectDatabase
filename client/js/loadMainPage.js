03761b27b9608e67ea635c985dd59c37374eb5e2
Steven Oh <stevenoh93@gmail.com>
2014-11-02 00:54:54 -0400

    loadMainPage working

--- client/js/loadMainPage.js
+++ client/js/loadMainPage.js
@@ -1,8 +1,12 @@
 //Script to load projects
+window.onload = loadMainPage
 
-document.getElementById("Item1").innerHTML = '<a href="project.html" class="thumb" title="An image"><img src="img/dummies/300x200d.gif" alt="Post" /></a> \
-	<div class="excerpt"> \
-		<a href="project.html" class="header">Lorem ipsum dolor</a> \
-		<a href="project.html" class="text">This is also changed by javascript</a> \
-		<div class="meta">Admin on 23 Jun, 2010</div> \
-	</div>'
+function loadMainPage() {
+	document.getElementById("Item1").innerHTML = 
+		'<a href="project.html" class="thumb" title="An image"><img src="img/dummies/300x200.gif" alt="Post" /></a> \
+			<div class="excerpt"> \
+				<a href="project.html" class="header">Lorem ipsum dolor</a> \
+				<a href="project.html" class="text">Changed content with javascript again!</a> \
+				<div class="meta">Admin on 23 Jun, 2010</div> \
+			</div>'
+}
