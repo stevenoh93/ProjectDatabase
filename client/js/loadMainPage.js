9819f5d411c9a4920e2b79040fcaffd0e7b7fa37
Steven Oh <stevenoh93@gmail.com>
2014-11-02 00:29:47 -0400

    Connect to MySQL working / Testing javascript

--- client/js/loadMainPage.js
+++ client/js/loadMainPage.js
@@ -1,2 +1,8 @@
 //Script to load projects
 
+document.getElementById("Item1").innerHTML = '<a href="project.html" class="thumb" title="An image"><img src="img/dummies/300x200d.gif" alt="Post" /></a> \
+	<div class="excerpt"> \
+		<a href="project.html" class="header">Lorem ipsum dolor</a> \
+		<a href="project.html" class="text">This is also changed by javascript</a> \
+		<div class="meta">Admin on 23 Jun, 2010</div> \
+	</div>'
