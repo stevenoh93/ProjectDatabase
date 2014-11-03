3a2ae008b6e3ea75fee6b8969715981921183627
Steven Oh <stevenoh93@gmail.com>
2014-11-02 23:06:19 -0500

    Populating tables with random data

--- server/ProjectDatabase/src/server/JdbcManager.java
+++ server/ProjectDatabase/src/server/JdbcManager.java
@@ -9,8 +9,14 @@ public class JdbcManager {
 			String mysqlServer = "72.76.204.54:3306/";
 			String dbName = "ece464";
 			con = DriverManager.getConnection ("jdbc:mysql://" + mysqlServer + dbName,"root","skdml!Tjqjdlek");
-			if(!con.isClosed())
+			if(!con.isClosed()) {
 				System.out.println("Successfully connected to MySQL server");
+				Statement s = con.createStatement();
+				RelationMaker rel = new RelationMaker(s);
+				//rel.populateTables("D:\\Projects\\ProjectDatabase\\Students.csv", "D:\\Projects\\ProjectDatabase\\Faculty.csv", "D:\\Projects\\ProjectDatabase\\Projects.csv", "D:\\Projects\\ProjectDatabase\\Courses.csv");
+				rel.relate();
+				s.close();
+			}
 		}
 		catch (Exception e) {
 			System.out.println(e.toString());
@@ -21,5 +27,7 @@ public class JdbcManager {
 					con.close();
 			} catch (SQLException e) {}
 		}
+		
+		
 	}
 }
