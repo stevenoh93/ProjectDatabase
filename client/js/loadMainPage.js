5d58c85c2b48331f5cfdc0e11c907d9bd331a1d6
Steven Oh <stevenoh93@gmail.com>
2014-11-03 00:52:10 -0500

    Playing with pager. Not working yet..

--- client/js/loadMainPage.js
+++ client/js/loadMainPage.js
@@ -1,7 +1,5 @@
 //Script to load projects
-window.onload = loadMainPage
-
-function loadMainPage() {
+$( document ).ready( = function {
 	document.getElementById("Item1").innerHTML = 
 		'<a href="project.html" class="thumb" title="An image"><img src="img/dummies/300x200.gif" alt="Post" /></a> \
 			<div class="excerpt"> \
@@ -9,4 +7,14 @@ function loadMainPage() {
 				<a href="project.html" class="text">Changed content with javascript again!</a> \
 				<div class="meta">Admin on 23 Jun, 2010</div> \
 			</div>'
-}
+});
+
+jQuery(function($) {
+	$(function() {
+		$("#pagination").pagination({
+			items: 100,
+			itemsOnPage: 10,
+			cssStyle: 'dark-theme'
+		});
+	});
+});
