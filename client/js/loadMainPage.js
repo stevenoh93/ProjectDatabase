c0afcf17a4e705610f499ced8ef1043bedf3c398
stevenoh93 <stevenoh93@gmail.com>
2014-11-04 15:09:08 -0500

    test witih ajax

--- client/js/loadMainPage.js
+++ client/js/loadMainPage.js
@@ -1,11 +1,34 @@
 //Script to load projects
 $( document ).ready(function() {
+	
+	loadXMLDoc();
 	$("#Item1").html(' \
 		<a href="project.html" class="thumb" title="An image"><img src="img/dummies/300x200.gif" alt="Post" /></a> \
-			<div class="excerpt"> \
-				<a href="project.html" class="header">Lorem ipsum dolor</a> \
-				<a href="project.html" class="text">Changed content with javascript again!</a> \
-				<div class="meta">Admin on 23 Jun, 2010</div> \
-			</div>' );
+							<div class="excerpt"> \
+								<a href="project.html" class="header">Lorem ipsum dolor</a> \
+								<a href="project.html" class="text">CHANGE</a> \
+								<div class="meta">Admin on 23 Jun, 2010</div> \
+							</div> \
+	');
 });
+
+function loadXMLDoc() {
+	var xmlhttp;
+	if (window.XMLHttpRequest) {
+	  // code for IE7+, Firefox, Chrome, Opera, Safari
+	  xmlhttp=new XMLHttpRequest();
+	}
+	else {
+	  // code for IE6, IE5
+	  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlhttp.onreadystatechange=function() {
+	  if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+		$("#Item1").html(xmlhttp.responseText);
+	  }
+	}
+	xmlhttp.open("GET","test.txt",true);
+	xmlhttp.send();
+	
+}
 
