1a8de142a4a24326e03b57a9a1da427ccf0923a6
Steven Oh <stevenoh93@gmail.com>
2014-11-04 08:04:08 -0500

    playing with pagination

--- client/js/loadMainPage.js
+++ client/js/loadMainPage.js
@@ -1,20 +1,16 @@
 //Script to load projects
-$( document ).ready( = function {
-	document.getElementById("Item1").innerHTML = 
-		'<a href="project.html" class="thumb" title="An image"><img src="img/dummies/300x200.gif" alt="Post" /></a> \
+$( document ).ready(function() {
+	$("#Item1").html(' \
+		<a href="project.html" class="thumb" title="An image"><img src="img/dummies/300x200.gif" alt="Post" /></a> \
 			<div class="excerpt"> \
 				<a href="project.html" class="header">Lorem ipsum dolor</a> \
 				<a href="project.html" class="text">Changed content with javascript again!</a> \
 				<div class="meta">Admin on 23 Jun, 2010</div> \
-			</div>'
+			</div>' );
 });
 
-jQuery(function($) {
-	$(function() {
-		$("#pagination").pagination({
-			items: 100,
-			itemsOnPage: 10,
-			cssStyle: 'dark-theme'
-		});
-	});
+
+$('#light-pagination').pagination({
+	pages: 20,
+	cssStyle: 'light-theme'
 });
