829c9e475b4a8162a42fd905cc3ddc1e3489b9e8
stevenoh93 <stevenoh93@gmail.com>
2014-11-12 19:45:38 -0500

    more on router

--- server/server.js
+++ server/server.js
@@ -7,9 +7,6 @@ function start(route) {
 		console.log("Request for " + pathname + " received.")
 
 		route(pathname, request, response);	
-
-		response.writeHead(200, {"Content-Type": "text/plain"});
-		response.write("Hello World");
 		response.end();
 	}).listen(8888);
 	console.log("Server has started");
