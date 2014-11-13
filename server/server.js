18572cd7975a1e0d4ce89f1e4a42662fdda9d069
Steven Oh <stevenoh93@gmail.com>
2014-11-13 08:15:03 -0500

    server testing with home desktop

--- server/server.js
+++ server/server.js
@@ -5,7 +5,11 @@ function start(route) {
 	http.createServer(function(request, response) {
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + " received.")
-
+		var body = 'hello world';
+		response.writeHead(200, {'Content-Type': 'text/plain',
+									 'Trailer': 'Content-MD5'  });
+		response.write('Hello World');
+		response.addTrailers({'Content-MD5': "7895bf4b8828b55ceaf47747b4bca667"});
 		route(pathname, request, response);	
 		response.end();
 	}).listen(8888);
